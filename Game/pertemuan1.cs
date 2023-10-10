cf23c790af1ecdda6c8d746a104557bdae35abfb
Muhammad Mirza Rafi'uddin <112935970+Mirza1805@users.noreply.github.com>
2023-10-10 19:30:13 +0700

    Add files via upload

---
+++ Game/pertemuan1.cs
@@ -0,0 +1,142 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("HALO");
+        /* 
+         string divisiDncc="HAHA";
+         Console.WriteLine(divisiDncc);
+         var div="YA";
+         var nomor='1';
+         Console.WriteLine(div);
+         Console.WriteLine(nomor);
+
+         const float phi=3.14f; //nilainya tetap
+         Console.WriteLine(phi);
+
+         int angkaint=088888;
+         float berat=59.77777777f;//kalau pakai float harus ditambah 'f' //maksimal 7
+         double brtt=12.3456789;//maksimal 16
+         Console.WriteLine(angkaint);
+         Console.WriteLine(berat);
+         Console.WriteLine(brtt);
+
+         string nama="Gajah";
+         string NIM="A11.2022.14344";
+         Console.WriteLine(nama);
+         Console.WriteLine(NIM);
+
+         string bulan = "januari \tFebruari \tmaret";
+         string hari = "senin \rselasa \rrabu";
+         Console.WriteLine(bulan);
+         Console.WriteLine(hari);
+
+         string txt1="ini text";
+         string txt2="ini juga text";
+         Console.WriteLine(txt1.ToUpper());
+         Console.WriteLine(txt2.ToLower());
+         Console.WriteLine(txt1.Length);
+         Console.WriteLine("Apa ini? " + txt1 + txt2);
+
+         string name="Difa";
+         var date = DateTime.Now;
+         Console.WriteLine($"Halo, {name}! hari ini adalah {date.DayOfWeek}, pukul {date:HH:mm} WIB.");
+         string firstname="Mirza";
+         string lastname="Rafi'uddin";
+         Console.WriteLine($"Halo nama saya {firstname} {lastname} dengan nim {NIM}");
+
+         bool jawaban=true;
+         if(jawaban==false){
+             Console.WriteLine("Benar");
+         }
+         else {
+             Console.WriteLine("salah");
+         }
+
+         float desimal=1.5f;
+         int convert=(int)desimal;
+         Console.WriteLine(convert);
+         int angka1=1;
+         float angka2=1.5f;
+         Console.WriteLine($"{angka1.ToString()} + {angka2.ToString()}");
+
+         string pertama = "5",kedua="7";
+         int sum = int.Parse(pertama) + int.Parse(kedua);
+         Console.WriteLine(sum);
+
+         Console.WriteLine("Masukan x = ");
+         int x = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Masukan y = ");
+         int y = Convert.ToInt32(Console.ReadLine());
+         var tambah=x+y;
+         var kurang=x-y;
+         var kali=x*y;
+         var bagi=x/y;
+         var modulo=x%y;
+         Console.WriteLine(tambah);
+         Console.WriteLine(kurang);
+         Console.WriteLine(kali);
+         Console.WriteLine(bagi);
+         Console.WriteLine(modulo);
+
+         Console.WriteLine("increment 1 :" + x );
+         Console.WriteLine(x++ );
+         Console.WriteLine(x);
+         Console.WriteLine("increment 2 :" + x );
+         Console.WriteLine(++x );
+         Console.WriteLine(x);
+          Console.WriteLine("decrement 1 :" + x );
+         Console.WriteLine(x-- );
+         Console.WriteLine(x);
+         Console.WriteLine("decrement 2 :" + x );
+         Console.WriteLine(--x );
+         Console.WriteLine(x);
+
+         Console.WriteLine(x+=3);
+
+         bool g = true;
+         bool h = false;
+         if (x<10 && y>5){
+             Console.WriteLine("True");
+         } 
+         else
+         Console.WriteLine("false");
+
+         string[]divisi = {"web", "mobile", "game", "mulmed", "jaringan"};
+         Console.WriteLine(divisi[0]);
+         Console.WriteLine(divisi[1]);
+         Console.WriteLine(divisi[2]);
+         Console.WriteLine(divisi[3]);
+         Console.WriteLine(divisi[4]);
+
+         int[] bilangan=new int[4];
+         //for (int i=0;i<4;i++){}
+         bilangan[0]=1;
+         bilangan[1]=3;
+         bilangan[2]=4;
+         bilangan[3]=6;
+         Console.WriteLine(bilangan[0]);
+         Console.WriteLine(bilangan[1]);
+         Console.WriteLine(bilangan[2]);
+         Console.WriteLine(bilangan[3]);
+
+         int[,]nubers={{1,2,3},{7,9,20}};
+         Console.WriteLine("array ke (0,2) : " + nubers[1,2]);
+
+         int[,,]numbers={{{1,2,3},{7,9,20}},{{21,34,51},{25,67,23}}};
+         Console.WriteLine("array ke (2,3) : " + numbers[1,1,2]);
+                 
+
+        var names = new List<string> { "game", "mobile", "web" };
+        for (int i = 0; i < names.Count; i++)
+        {
+            Console.Write(names[i] + " ");
+        }*/
+
+    }
+}
